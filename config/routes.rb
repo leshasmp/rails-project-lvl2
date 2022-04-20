47f38ab86758ed8fb0cf3e67272e7de10aefeb60
alexey <alexey-melekhov1989@yandex.ru>
2022-04-21 01:02:00 +0500

    change style hash in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   scope module: :web do
     root 'home#index'
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: %i[index show new create] do
       scope module: :posts do
-        resources :comments, only: [:create]
-        resources :likes, only: [:create, :destroy]
+        resources :comments, only: %i[create]
+        resources :likes, only: %i[create destroy]
       end
     end
   end
