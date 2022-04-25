67a3d348d7b66c63d62bc4302de8d125737ebbf1
alexey <alexey-melekhov1989@yandex.ru>
2022-04-25 17:49:46 +0500

    change column name user in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :post_category
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id', inverse_of: :posts
   has_many :post_comments, dependent: :destroy
   has_many :post_likes, dependent: :destroy
 
